010ac08f990916c78881a50376c26080ad381c09
Philip McCullick <pcmccull@nps.edu>
2014-05-21 12:58:50 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 multi-column-list
 =================
 
-A jQuery plugin for turning a normal list into a multiple column list, where each column is a fixed width
+A jQuery plugin for turning a normal list into a responsive multiple column list, where each column is a fixed width. The number of columns changes based on the container width and responds with the window resizing.
+
+Basic usage:
+$(".multiColumnedList").multiColumnLists({
+    columnWidth: 250,
+    containerEl: $("body")
+});
+
+Options:
+- columnWidth = the number of pixels each column should be
+- containerEl = the container that should used for resizing
---
+++ multiColumnList.js
@@ -0,0 +1,59 @@
+/* Multicolumn list plugin, CED3, Naval Postgraduate School, May 2014
+ * Converts a UL LI list into multiple columns. The number of columns is 
+ * determined by the width of the container element diveded by the width of each column.
+ * 
+ * Usage: 
+ *  $("#multiColumnedList").multiColumnLists({
+        columnWidth: 150,
+        containerEl: $("body")
+    });
+ * 
+ * @author Philip McCullick
+ * */
+;(function ($) {
+    $.fn.extend( {
+        multiColumnLists: function (options) {
+            this.defaults = {
+                    columnWidth: 150,
+                    containerEl: $("body")
+            };
+
+            var settings = $.extend( {}, this.defaults, options );
+
+            return this.each(function() {
+                var $el = $(this);
+                var listEl = $el.find("> ul");
+                var listItems = [];
+                //cache the list items
+                listEl.find("> li").each(function () {
+                    listItems.push("<li>" + $(this).html() + "</li>");
+                });
+                var widthDiff = listEl.outerWidth() - listEl.width();
+                
+                var redrawList = function () {      
+                    var columns = Math.max(1, Math.floor(options.containerEl.width() / (options.columnWidth )));                    
+                    var itemsPerColumn = Math.floor(listItems.length / columns);
+                    var extraItems = listItems.length % columns;
+                    var output = "";
+                    var currentItem = 0;
+                    for (var i = 0; i < columns; i++) {
+                        output += "<ul class='multiColumnListColumn' style='width:" + (options.columnWidth - widthDiff) + "px'>";
+                        var startIndex = currentItem;
+                        var columnLength = itemsPerColumn + (i <extraItems?1:0);
+                        console.log(columnLength, extraItems);
+                        for (var j = 0; j < columnLength; j++) {
+                            output +=  listItems[j + startIndex];
+                            
+                            currentItem++;
+                        }
+                        output += "</ul>";
+                    }
+                    $el.html(output);
+                };
+                redrawList(); 
+                $(window).on("resize", redrawList);
+            });
+        }
+    });
+    
+})(jQuery);
